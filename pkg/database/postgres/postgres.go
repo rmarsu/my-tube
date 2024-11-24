6777b21af8b81605cded7fa8019ab718cb8a2fcc
rmarsu <r.rmarsu@gmail.com>
2024-11-24 13:30:51 +0300

    public repo

--- pkg/database/postgres/postgres.go
+++ pkg/database/postgres/postgres.go
@@ -3,12 +3,13 @@ package database
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func Connect() (*pgxpool.Pool, error) {
-	dbpool, err := pgxpool.New(context.Background(), "postgres://postgres:qwerty@localhost:5432/postgres?sslmode=disable")
+	dbpool, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to the database: %w", err)
 	}
